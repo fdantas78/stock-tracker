57b9059d28e63a512e32ed5f9bb24a1672f62d28
fdantas78 <development@dantas.me>
2016-11-29 19:37:10 +0000

    Update views, navigation and stocks show order by Ticker

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,8 @@ class Stock < ActiveRecord::Base
     has_many :user_stocks
     has_many :users, through: :user_stocks
     
+    default_scope { order(ticker: :asc) }
+    
     def self.find_by_ticker(ticker_symbol)
         where(ticker: ticker_symbol).first
     end
