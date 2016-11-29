725277a2dfa0092bca0e3aa8a400a8f07f7a6ae5
fdantas78 <development@dantas.me>
2016-11-29 17:58:40 +0000

    Add user and stock association

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -17,6 +19,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :user_stocks, except: [:show, :edit, :update]
 
   # Example resource route with options:
   #   resources :products do
